609d3999a374a463e2ba45eb1e3c3e219c7228aa
Mo <domo5@web.de>
2023-11-22 15:29:10 +0100

    d

--- fetch.js
+++ fetch.js
@@ -2,5 +2,7 @@ async function getJson (){
     const response = await fetch("test.json");
     const data = await response.json();
     return data;
+   
 }
+
 getJson().then(data=>console.log(data));
