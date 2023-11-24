fc61c69fb7f03e9613ce3a4bd9b419821031f620
Mo <domo5@web.de>
2023-11-24 10:13:17 +0100

    clean

--- console.js
+++ console.js
@@ -1,41 +1,14 @@
-
-const Array= {
-    
-    "laender": [
-        {"land": "Volksrepublik China", "CO2": "11535" },
-        {"land": "Vereinigte Staaten von Amerika", "CO2": "5107" },
-        {"land": "Indien", "CO2": "2597" },
-        {"land": "Russland", "CO2": "1792" },
-        {"land": "Japan", "CO2": "1154" },
-        {"land": "Deutschland", "CO2": "702" },
-        {"land": "Iran", "CO2": "701" },
-        {"land": "Südkorea", "CO2": "352" },
-        {"land": "Indonesien", "CO2": "626" },
-        {"land": "Saudi-Arabien", "CO2": "615" }
-        ],
-
-     "unternehmen": [
-        {"unternehmen" : "RWE", "CO2" : "118"},
-        {"unternehmen" : "HeidelbergCement", "CO2" : "73"},
-        {"unternehmen" : "E.ON", "CO2" : "69"},
-        {"unternehmen" : "Lufthansa", "CO2" : "33"},
-        {"unternehmen" : "Eskom", "CO2" : "216"},
-        {"unternehmen" : "Gazprom", "CO2" : "111"},
-        {"unternehmen" : "Enel", "CO2" : "107"},
-        {"unternehmen" : "Tepco", "CO2" : "71"},
-        {"unternehmen" : "Ameren", "CO2" : "60"},
-        {"unternehmen" : "FirstEnergy", "CO2" : "63"}
-
-        ]
-}
-
-function bla(){
-let speicher ="";
-
-    for (let element of Array.laender){
-        speicher += element.land;
-        console.log(speicher); 
+async function getFile(){
+    let eintrag ="";
+    let Header = document.getElementById("myTable")
+    let response =  await fetch("test.json");
+    let json = await response.json();
+    let i =0;
+    for (let element of json){
+        eintrag +=` <tr> <th scope="row">${i}</th><td>${element.name} </td> <td> ${element.age} </td> </tr>`
+        i++;
     }
-    return speicher;
-    
+    Header.innerHTML = eintrag;
+
+
 }
--- fetch.js
+++ fetch.js
@@ -1,45 +1,17 @@
 function createTable(){
-    fetch("daten.json")
+    fetch("test.json")
     .then(function(response){
         return response.json();
     })
-    .then(function(laender){
-        let tabelle = document.queryselector("#Tabelle");
+    .then(function(test){
+        let tabelle = document.queryselector("#Text");
         let out ="";
         for ( let element of array){
-            out += `<tr> <td> ${element.name} </td> </tr> `; 
+            out += element.name; 
         }
+        tabelle.innerHTML = out;
     })
-}
-
-
-function getJson2 (){
-    fetch("sample-file.json")
-     .then(response=> response.json())
-         .then(data=> { console.log(data)
-
-            
- })
-   
-}
-
-
-function getText(){
-
-    fetch("sample-file.json")
-    .then(response=> response.json())
-        .then(data=> {console.log(data)
-           return data
     
-        })
-    }
-
-
-
-    function getHallo(){
-
-       return "Hallo"
-    }
-    
+}
 
 
