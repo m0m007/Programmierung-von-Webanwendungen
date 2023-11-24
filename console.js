3c521c7bb03c560f747ee019af3a99a23aff987a
Mo <domo5@web.de>
2023-11-24 12:50:21 +0100

    d

--- console.js
+++ console.js
@@ -1,12 +1,11 @@
 async function getFile(){
     let eintrag ="";
     let Tabelle = document.getElementById("myTable");
-    let response =  await fetch("test.json");
+    let response =  await fetch("./test.json");
     let json = await response.json();
-    let i =0;
     for (let element of json){
         eintrag +=` <tr> </th><td>${element.name} </td> <td> ${element.age} </td> </tr>`
-        i++;
+    
     }
     Tabelle.innerHTML = eintrag;
 
@@ -16,4 +15,21 @@ async function getFile(){
 function getHeader(){
     let Header =document.getElementById("Text");
     Header.innerHTML= "Hallo"
+}
+
+
+function getFile2(){
+    let eintrag ="";
+    let Tabelle = document.getElementById("myTable");
+    fetch("test.json")
+	    .then(function(response){
+            return response.json();
+        })
+        .then(function (data){
+            for (let element of data){
+              eintrag +=` <tr> </th><td>${element.name} </td> <td> ${element.age} </td> </tr>`;
+            }
+        Tabelle.innerHTML = element;
+    }); 
+
 }
