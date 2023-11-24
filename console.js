c6a69cbc18818fe6bcc1ddc62dcff61acf80c498
Mo <domo5@web.de>
2023-11-24 10:36:53 +0100

    d

--- console.js
+++ console.js
@@ -5,7 +5,7 @@ async function getFile(){
     let json = await response.json();
     let i =0;
     for (let element of json){
-        eintrag +=` <tr> <th scope="row">${i}</th><td>${element.name} </td> <td> ${element.age} </td> </tr>`
+        eintrag +=` <tr> </th><td>${element.name} </td> <td> ${element.age} </td> </tr>`
         i++;
     }
     Tabelle.innerHTML = eintrag;
